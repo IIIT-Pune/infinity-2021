715831a08b6bf4416e1c139930bca80167e12975
Rushikesh Tote <rushi.tote@gmail.com>
2021-12-02 14:42:34 +0530

    added team section

---
+++ data/TeamMembers.js
@@ -0,0 +1,88 @@
+const TeamMembers = [
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  },
+  {
+    name: "John Doe",
+    work: "Web Designer",
+    github: "https://github.com/john",
+    linkedin: "https://linkedin.com/john",
+    instagram: "https://instagram.com/john",
+  }
+]
+
+export default TeamMembers
