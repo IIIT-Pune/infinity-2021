04bea277a4c2695c8cf1acc325f4c49b2603bf24
Mukhrit <54068781+Mukhrit@users.noreply.github.com>
2021-12-03 23:13:11 +0530

    about us page done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Team from "./Team";
+import AboutUs from "./components/AboutUs";
 import "./App.css";
 
 import "./styles/tailwind.css";
@@ -11,7 +12,9 @@ function App() {
 		<div className="bg-black min-h-screen h-full">
 			<NavBar />
 			<Home/>
+			<AboutUs/>
 			<Team />
+
 		</div>
 	);
 }
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,34 @@
+import mask from '../assets/front man mask tp 1.png'
+function AboutUs() {
+  return (
+    <div className="p-16">
+        <div className="grid lg:grid-cols-2"> 
+        <div className="flex-col space-y-8">
+        <div className="uppercase text-5xl font-black text-center md:text-left">
+        <span className="text-white">
+            About
+        </span>{' '}
+        <span className="text-pink">
+            Us
+        </span>
+        </div>
+        <div className="font-montserrat font-normal text-xl text-center md:text-left lg:text-3xl ">
+        <span className="text-white">
+            Infinity is IIIT Pune's annual flagship coding contest hosted every year on Codechef. A 3 hour coding contest that's guaranteed to get your head itching and your heart wanting more. Infinity started in 2017 and was limited only to IIIT Pune.
+        </span>
+        <br/>
+        <br/>
+        <span className="text-white">
+           The contest follows ACM style scoring and consists of problems designed for participants ranging from beginners to experts. Anyone having a knack for coding is encouraged to participate.
+        </span>
+        </div>
+        </div>
+        <div>
+            <img src={mask} alt="..."></img>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default AboutUs;
