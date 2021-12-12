f0974cce8d36e9ea54cf8769d95b5687601e0742
Rushikesh Tote <rushi.tote@gmail.com>
2021-12-12 17:32:21 +0530

    added archives page

---
+++ src/data/ProblemSet.js
@@ -0,0 +1,46 @@
+const problemSet = [
+  {
+    code: "EOOPR",
+    name: "The One Who Knocks!",
+    link: "https://www.codechef.com/INFY20B/problems/EOOPR",
+    successfulSubmissions: 647,
+    accuracy: 8.74,
+  },
+  {
+    code: "SOLDVAL",
+    name: "Empire Business",
+    link: "https://www.codechef.com/INFY20B/problems/SOLDVAL",
+    successfulSubmissions: 194,
+    accuracy: 11.02,
+  },
+  {
+    code: "RRUN",
+    name: "Los Pollos Hermanos",
+    link: "https://www.codechef.com/INFY20B/problems/RRUN",
+    successfulSubmissions: 103,
+    accuracy: 23.13,
+  },
+  {
+    code: "GMSTN",
+    name: "Better call Saul!",
+    link: "https://www.codechef.com/INFY20B/problems/GMSTN",
+    successfulSubmissions: 27,
+    accuracy: 8.88,
+  },
+  {
+    code: "KTREE",
+    name: "Morosoph Todd",
+    link: "https://www.codechef.com/INFY20B/problems/KTREE",
+    successfulSubmissions: 4,
+    accuracy: 9.52,
+  },
+  {
+    code: "KGCD",
+    name: "The Chosen One",
+    link: "https://www.codechef.com/INFY20B/problems/KGCD",
+    successfulSubmissions: 1,
+    accuracy: 2.13,
+  }
+]
+
+export default problemSet
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
+			spacing: {
+				"78": "19.5rem",
+			},
 			colors: {
 				pink: "#BE3A6F",
+				archivePrimary: "#DB5370",
+				archiveSecondary: "#39101D",
+				archiveLight: "#F4CED6",
+				archiveDark: "#C95782"
 			},
 			fontFamily: {
 				montserrat: ["Montserrat", "sans-serif"],
