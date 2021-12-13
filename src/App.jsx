dd84547ed02215d4885a87be24cd217566d0f826
Rakshit Jain <rakshitdhoka13@gmail.com>
2021-12-13 11:11:51 +0530

    some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-
 import Home from "./components/Home";
-import NavBar from "./components/NavBar";
 import AboutUs from "./components/AboutUs";
+import NavBar from "./components/NavBar";
+import Archive from "./components/Archive";
 import Team from "./components/Team";
 import "./App.css";
 
@@ -12,10 +12,11 @@ function App() {
 	return (
 		<div className="bg-black min-h-screen h-full">
 			<NavBar />
-			<Home/>
+			{/* <Home/>
 			<AboutUs/>
-			<Team />
-			<Footer/>
+			<Team /> */}
+			<Archive />
+			<Footer />
 		</div>
 	);
 }
--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -15,62 +15,62 @@ function ProblemCard(props) {
     accuracy = props.problem.accuracy;
 
   return (
-    <div className="max-w-sm rounded bg-gradient-to-b from-archiveLight to-archiveDark h-80 w-52">
-      {isHidden ? (
-        <div
-          className="flex flex-col rounded gap-4 justify-center items-center m-1 bg-archiveSecondary min-w-max h-78"
-          onClick={() => {
-            setIsHidden(false);
-          }}
-        >
-          <BsSquare className="text-6xl bg-none text-white" />
-          <BsTriangle className="text-6xl bg-none text-white" />
-          <BsCircle className="text-6xl bg-none text-white" />
-        </div>
-      ) : (
-        <div className="flex flex-col rounded gap-2 justify-center items-center m-1 bg-archivePrimary min-w-max h-78">
-          <a
-            href={problemLink}
-            target="_blank"
-            className="flex flex-col justify-center items-center"
-          >
-            <div className="text-black font-extrabold text-3xl">
-              {problemCode}
-            </div>
-            <div className="text-black font-bold">{problemName}</div>
-          </a>
-          <div className="text-white font-extrabold text-3xl">
-            {successfulSubmissions}
-          </div>
-          <div className="text-white font-bold">Successful Submissions</div>
-          <div style={{ width: 120, height: 120, fontWeight: "bolder" }}>
-            <CircularProgressbar
-              value={accuracy}
-              text={`${accuracy}`}
-              background
-              backgroundPadding={6}
-              styles={buildStyles({
-                backgroundColor: "#F9E7E8",
-                textColor: "#BF4A70",
-                pathColor: "#BF4A70",
-                trailColor: "transparent",
-              })}
-            />
-          </div>
-          <div className="text-white font-bold">Accuracy</div>
-        </div>
-      )}
-    </div>
-  );
+		<div className="max-w-sm rounded bg-gradient-to-b from-archiveLight to-archiveDark h-80 w-52">
+			{isHidden ? (
+				<div
+					className="flex flex-col rounded gap-4 justify-center items-center m-1 bg-archiveSecondary min-w-max h-78"
+					onClick={() => {
+						setIsHidden(false);
+					}}
+				>
+					<BsSquare className="text-6xl bg-none text-white" />
+					<BsTriangle className="text-6xl bg-none text-white" />
+					<BsCircle className="text-6xl bg-none text-white" />
+				</div>
+			) : (
+				<div className="flex flex-col rounded gap-2 justify-center items-center m-1 bg-archivePrimary min-w-max h-78 ">
+					<a
+						href={problemLink}
+						target="_blank"
+						className="flex flex-col justify-center items-center text-black hover:text-white/60 transition duration-150"
+					>
+						<div className=" font-extrabold text-3xl ">
+							{problemCode}
+						</div>
+						<div className="font-bold">{problemName}</div>
+					</a>
+					<div className="text-white font-extrabold text-3xl">
+						{successfulSubmissions}
+					</div>
+					<div className="text-white font-bold">Successful Submissions</div>
+					<div style={{ width: 120, height: 120, fontWeight: "bolder" }}>
+						<CircularProgressbar
+							value={accuracy}
+							text={`${accuracy}`}
+							background
+							backgroundPadding={6}
+							styles={buildStyles({
+								backgroundColor: "#F9E7E8",
+								textColor: "#BF4A70",
+								pathColor: "#BF4A70",
+								trailColor: "transparent",
+							})}
+						/>
+					</div>
+					<div className="text-white font-bold">Accuracy</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 function ProblemSet(props) {
   return (
     <a href={props.link} target="_blank">
       <div className="w-72 md:w-96 h-60 bg-archivePrimary text-white flex flex-col rounded gap-4 justify-center items-center">
-        <img src={CodeChefLogo} className="w-40 h-40" />
-        <div className="self-end mr-3 text-4xl font-extrabold">
-          {props.year}
+        <img src={CodeChefLogo} className="w-40 h-40 bg-transparent" />
+        <div className=" text-4xl font-extrabold text-center">
+          {props.year} Infinity
         </div>
       </div>
     </a>
@@ -95,7 +95,7 @@ function Archive() {
       <div className="flex justify-center items-center flex-wrap gap-4 text-white text-6xl font-extrabold mb-10">
         QUESTION<div className="text-archivePrimary">SET</div>
       </div>
-      <div className="max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="max-w-6xl ml-auto mr-auto flex flex-col md:flex-row items-center gap-y-8  md:justify-around mb-5">
         <ProblemSet year="2K19" link="https://www.codechef.com/INFY2019/" />
         <ProblemSet year="2K18" link="https://www.codechef.com/INTY2018" />
       </div>
